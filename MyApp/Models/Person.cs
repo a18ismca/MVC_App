f72887a72274f89fdf8704af191680e14a999720
a18ismca <ismetcatovic@hotmail.se>
2022-09-08 11:54:52 +0200

    Add project files.

---
+++ MyApp/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace MyApp.Models
+{
+    public class Person
+    {
+        public string SSN { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public List<Vehicle> OwnedVehicles { get; set; } = new List<Vehicle>();
+        
+        public string VehiclePlate { get; set; }
+    }
+}
